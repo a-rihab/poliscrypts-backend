025e831a85c9318823b983b530793289746397b6
a.lamour <dev.inf@hotmail.fr>
2022-02-22 10:45:52 +0100

    fix the lombok problem

--- src/main/java/com/poliscrypts/security/UserDetailsImpl.java
+++ src/main/java/com/poliscrypts/security/UserDetailsImpl.java
@@ -10,12 +10,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import com.poliscrypts.model.User;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
