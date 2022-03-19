ad7d05debcc9662466d62fe2cd295bdfe4b48f1c
a.lamour <dev.inf@hotmail.fr>
2022-03-19 23:30:58 +0100

    fix the swagger problem

--- src/main/java/com/poliscrypts/config/WebSecurityConfig.java
+++ src/main/java/com/poliscrypts/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -50,6 +51,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	@Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/swagger-ui/**", "/v3/api-docs/**");
+    }
+	
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http	.cors()
 				.and()
@@ -60,11 +66,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.and()
 				.authorizeRequests()
 				
-				.antMatchers("/swagger-ui/*").permitAll()
+				.antMatchers("/v2/api-docs",
+	                    "/configuration/ui",
+	                    "/swagger-resources/**",
+	                    "/configuration/security",
+	                    "/swagger-ui/**",
+	                    "/webjars/**").permitAll()
 				.antMatchers("/api/login").permitAll()
 				.antMatchers("/api/users").permitAll()
 				.antMatchers("/api/refresh").permitAll()
 				.antMatchers("/h2-ui/*").permitAll()
+				
 				
 				.anyRequest()
 				.authenticated();
